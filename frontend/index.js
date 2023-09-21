b0abc4c9665e6fc69d4faee8b696557f81585fcc
Michael MacDougall <mgmacdougall@outlook.com>
2023-09-21 12:29:28 -0400

    Add of changes to frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,14 +1,14 @@
-const submissionForm = document.getElementById("submission-form");
-const rootNode = document.getElementById("root");
-const homePageSection = document.querySelectorAll("section")[0];
-const recListEl = document.getElementById("rec-list");
+const submissionForm = document.getElementById('submission-form');
+const rootNode = document.getElementById('root');
+const homePageSection = document.querySelectorAll('section')[0];
+const recListEl = document.getElementById('rec-list');
 
 const resetRecipeCardDetails = () => {
-  const recipeDetailsEl = document.getElementById("rec-details");
-  recipeDetailsEl.innerHTML = "";
+  const recipeDetailsEl = document.getElementById('rec-details');
+  recipeDetailsEl.innerHTML = '';
 };
 
-const recipeButtonListener = async (e) => {
+const recipeButtonListener = async e => {
   if (e.target.dataset.id) {
     const recipeInfo = await fetchRecipeDetailsById(e.target.dataset.id);
     renderChange();
@@ -16,26 +16,26 @@ const recipeButtonListener = async (e) => {
   }
 };
 
-const getRecipeImage = async (id) => {
+const getRecipeImage = async id => {
   const recipeImage = await fetchRecipeImageById(id);
   return recipeImage;
 };
 
-const renderRecipe = async (recipe) => {
+const renderRecipe = async recipe => {
   resetRecipeCardDetails();
   const image = await getRecipeImage(recipe.id);
   const imageObjectURL = URL.createObjectURL(image);
-  const recipeDetailsEl = document.getElementById("rec-details");
+  const recipeDetailsEl = document.getElementById('rec-details');
   recipeDetailsElInnerHtml = `
     <img src=${imageObjectURL} class="img-fluid" style="width:100%;" alt="...">
   `;
   const recipeSteps = recipe.steps
     .map(
-      (step) => `
+      step => `
       <li class="list-group-item">${step}</li>
   `
     )
-    .join("");
+    .join('');
 
   const recipeHtml = `
   <div class="container my-5">
@@ -53,38 +53,36 @@ const renderRecipe = async (recipe) => {
   recipeDetailsEl.innerHTML = recipeHtml;
 };
 
-const fetchRecipeDetailsById = async (id) => {
+const fetchRecipeDetailsById = async id => {
   const details = await fetch(`http://localhost:5454/${id}`);
   const data = await details.json();
   return data;
 };
 
-const fetchRecipeImageById = async (id) => {
+const fetchRecipeImageById = async id => {
   const details = await fetch(`http://localhost:5454/img/${id}`);
   return details.blob();
 };
 
 const renderChange = () => {
-  homePageSection.classList.add("hidden");
-  document.querySelector("[href='/edit'").classList.toggle('hidden')
+  homePageSection.classList.add('hidden');
+  document.querySelector("[href='/edit'").classList.toggle('hidden');
 };
 
 async function populateData() {
-  const { udata } = await loadData();
+  const {udata} = await loadData();
   const d = udata.map((data, index) => {
     return `<div class="col-md-6 mt-2" >
-        <div class="${
-          index % 2 === 0
-            ? "h-100 p-5 text-bg-dark rounded-3"
-            : "h-100 p-5 bg-body-tertiary border rounded-3"
-        }">
-          <h2>${data.name}</h2>
+    <div class="${index % 2 === 0
+      ? 'h-100 p-5 text-bg-dark rounded-3'
+      : 'h-100 p-5 bg-body-tertiary border rounded-3'}">
+      <h2>${data.name}</h2>
+   
+      
           <p>${data.details}</p>
-          <button class="${
-            index % 2 === 0
-              ? "btn btn-outline-light"
-              : "btn btn-outline-secondary"
-          } type="button" data-id=${data.id}>More...</button>
+          <button class="${index % 2 === 0
+            ? 'btn btn-outline-light'
+            : 'btn btn-outline-secondary'} type="button" data-id=${data.id}>More...</button>
         </div>
       </div>
       `;
@@ -93,26 +91,25 @@ async function populateData() {
   rootNode.innerHTML = `
   <div class="container py-4">
     <div class="row align-items-md-stretch">
-      ${d.join("")}
+      ${d.join('')}
     </div>
   </div>
 `;
 }
 
 async function loadData() {
-  const data = await fetch("http://localhost:5454/");
+  const data = await fetch('http://localhost:5454/');
   return data.json();
 }
 
 const addRecipe = () => {};
-document.addEventListener("DOMContentLoaded", populateData);
-document.addEventListener("click", recipeButtonListener);
+document.addEventListener('DOMContentLoaded', populateData);
+document.addEventListener('click', recipeButtonListener);
 
 const renderRecipeForm = () => {
-
   // Disable the 'Add...' button.
-  document.querySelector("[href='/edit'").classList.toggle('hidden')
-  const containerEl = document.getElementById("edit");
+  document.querySelector("[href='/edit'").classList.toggle('hidden');
+  const containerEl = document.getElementById('edit');
   const containerFormEl = `
     <div class="px-4 py-5 my-5 text-center">
         <form>
@@ -131,38 +128,38 @@ const renderRecipeForm = () => {
   containerEl.innerHTML = containerFormEl;
 };
 
-document.querySelector(".nav").addEventListener("click", (e) => {
+document.querySelector('.nav').addEventListener('click', e => {
   e.preventDefault();
-  if (e.target.innerText.toLowerCase().includes("add")) {
-    rootNode.innerHTML = "";
-    recListEl.classList.toggle("hidden");
+  if (e.target.innerText.toLowerCase().includes('add')) {
+    rootNode.innerHTML = '';
+    recListEl.classList.toggle('hidden');
     renderRecipeForm(e.target);
   }
-  if (e.target.innerText.toLowerCase().includes("home")) {
-    location.replace("/");
+  if (e.target.innerText.toLowerCase().includes('home')) {
+    location.replace('/');
   }
 });
 
-document.querySelector("body").addEventListener("submit", async (e) => {
+document.querySelector('body').addEventListener('submit', async e => {
   e.preventDefault();
-  const title = document.getElementById("title").value;
-  const steps = document.getElementById("floatingTextarea").value;
-  const data = { name: title, steps: steps };
+  const title = document.getElementById('title').value;
+  const steps = document.getElementById('floatingTextarea').value;
+  const data = {name: title, steps: steps};
 
   const options = {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json'
     },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
   };
 
   fetch(`http://localhost:5454/postDetails`, options).then(() => {
-    location.replace("/");
+    location.replace('/');
   });
 });
